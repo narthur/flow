c294871b4edbc70977dc856961ad3d8acbef98aa
Nathan Arthur <nathan@nathanarthur.com>
2025-01-21 15:29:43 -0500

    Update +page.svelte

--- src/routes/tasks/+page.svelte
+++ src/routes/tasks/+page.svelte
@@ -74,16 +74,32 @@
 					</div>
 				</div>
 
-				<div class="mt-4 flex gap-6 text-sm text-neutral-600">
-					<div>
-						<span class="font-medium">{task.stats.sessionCount}</span> sessions
+				<div class="mt-4 flex items-center justify-between">
+					<div class="flex gap-6 text-sm text-neutral-600">
+						<div>
+							<span class="font-medium">{task.stats.sessionCount}</span> sessions
+						</div>
+						<div>
+							<span class="font-medium">{task.stats.totalMinutes}</span> minutes
+						</div>
+						<div>
+							<span class="font-medium">{task.stats.postponeCount}</span> postponements
+						</div>
 					</div>
-					<div>
-						<span class="font-medium">{task.stats.totalMinutes}</span> minutes
-					</div>
-					<div>
-						<span class="font-medium">{task.stats.postponeCount}</span> postponements
-					</div>
+
+					<button
+						class="text-neutral-400 hover:text-neutral-600 transition-colors duration-200"
+						on:click={() => {
+							if (confirm('Are you sure you want to delete this task?')) {
+								tasks.deleteTask(task.id);
+							}
+						}}
+						title="Delete task"
+					>
+						<svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+							<path fill-rule="evenodd" d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z" clip-rule="evenodd" />
+						</svg>
+					</button>
 				</div>
 
 				{#if task.postponedUntil}
